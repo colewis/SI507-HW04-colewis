ae49aa1ba176b5679b44e42546ecc2ae7341e0cb
colby lewis <colbylillewis@0587350424.wireless.umich.net>
2018-01-29 14:10:29 -0500

    colby's

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,10 @@
 def ask_question():
-    question = input("What is your question?")
-    #answers = []
-    #answers.append(question)
+    question = 0
+    while question != 'quit':
+        question = input("What is your question? ")
+        if question[-1] != '?':
+            print("I'm sorry, I can only answer questions.")
+
 
 m8 = ask_question()
 print(m8)
